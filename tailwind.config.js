79a76e8a05e5d92254b726796bd4f5aa8525ad80
Nicolai Verheul <n.verheul@student.science.ru.nl>
2022-12-11 16:10:52 +0100

    changed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export const content = [
   './pages/**/*.{js,ts,jsx,tsx}',
-  './Components/**/*.{js,ts,jsx,tsx}',
+  './components/**/*.{js,ts,jsx,tsx}',
 ];
 export const theme = {
   extend: {
